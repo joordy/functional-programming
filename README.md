12e16be0d3c29ea6e557e779016fe6099d7b9b2b
joordy <jmfronik@gmail.com>
2020-10-21 15:17:11 +0200

    Filtered the shoe sizes out of the array with .map

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # functional-programming
+
+### Description
+
+<!-- Wat doet je project (description) -->
+
+### Features
+
+<!-- Welke features zijn er (features) -->
+
+### Dataset
+
+<!-- Welke data gebruik je (dataset) -->
+
+### Install
+
+<!-- Hoe draai ik je project (install) -->
+
+### Deploy
+
+<!-- Live link van je project (deploy) -->
+
+### Bronnen
+
+<!-- credits en license (bronnen) -->
+
+---
+
+## TO have in wiki
+
+<!-- concept (schertsen, interface, iteraties)
+Research (programmeer principes)
+Data vrzamelen (query's, endpoints)
+Data pschonen (filter, clean)
+Logboek (standup, 1 op 1 gesprekken) -->
--- local_server/js/script.js
+++ local_server/js/script.js
@@ -4,12 +4,38 @@ console.log('works');
 
 // Use fetch() method to load data, only accessible with the server.
 // Async function to acces data outside the fetch function.
-async function fetchMoviesJSON(allUsers) {
+async function fetchUserData(allUsers) {
   let res = await fetch('../data/userData.json');
-  let allUsers = await res.json();
+  allUsers = await res.json();
+  // console.log(allUsers);
   return allUsers;
 }
 
-fetchMoviesJSON().then((allUsers) => {
-  console.log(allUsers[0].mensenAangesprokenMetU);
+fetchUserData().then((allUsers) => {
+  let columnName = 'schoenmaat';
+  let answerList = [];
+
+  // print all the shoe sizes itemshj
+  allUsers.map((answer) => {
+    answerList.push(answer[columnName]);
+    // Push elements into HTML with individual List items.
+    let node = document.createElement('li');
+    let textNode = document.createTextNode(answer[columnName]);
+    node.appendChild(textNode);
+    document.body.appendChild(node);
+  });
+  // allUsers.forEach((answer) => {
+  //   answerList.push(answer[columnName]);
+  //   // Push elements into HTML with individual List items.
+  //   let node = document.createElement('li');
+  //   let textNode = document.createTextNode(answer[columnName]);
+  //   node.appendChild(textNode);
+  //   document.body.appendChild(node);
+  // });
+  // console.log(answerList);
+
+  // // Parse the number of cijfergezondheid to integers
+  // // Reset of the variables to reuse them again
+  // columnName = 'cijferGezondheid';
+  // answerList = [];
 });
