021089c4cab3a6d26b81b0950374f5136657fbee
joordy <jmfronik@gmail.com>
2020-10-29 19:52:55 +0100

    Transfer from static to parcel-bundler, to use JS modules

---
+++ src/scripts/index.js
@@ -0,0 +1,28 @@
+import './../scss/index.scss';
+import { endpoint } from './modules/getData';
+console.log('hello');
+
+let selectedColumn = null;
+
+getData(endpoint).then((data) => {
+  console.log('hello, data is loaded');
+  console.log(data);
+  // selectedColumn = 'areaid';
+  // const areaManagerID = getAreaManager(data, selectedColumn);
+  // console.log(areaManagerID);
+});
+
+async function getData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    err;
+  }
+}
+
+// // Place all Area Manager ID's inside an new array
+// function getAreaManager(dataArray, key) {
+//   return dataArray.map((item) => item[key]);
+// }
---
+++ src/scripts/modules/getData.js
@@ -0,0 +1,3 @@
+// export const endpoint = ' https://opendata.rdw.nl/resource/t5pc-eb34.json ';
+export const endpoint = 'modules/data/userData.json';
+//let selectedColumn = null;
